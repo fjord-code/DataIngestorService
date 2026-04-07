b984145374bfe84967cd3ddf6a7052735214c7a5
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2026-04-07 22:30:14 +0300

    Project setup (#1)
    * Added gitignore.
    
    * Added ci pipeline.
    
    * Initialize project structure.

---
+++ src/DataIngestorService.Core/Contracts/Options/WeakAppOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace DataIngestorService.Core.Contracts.Options;
+
+public class WeakAppOptions
+{
+    public const string SectionName = "WeakApp";
+    public string BaseUrl { get; set; } = string.Empty;
+    public int TimeoutSeconds { get; set; } = 5;
+    public int RetryCount { get; set; } = 3;
+    public int CircuitBreakerFailureCount { get; set; } = 5;
+    public int CircuitBreakerBreakDurationSeconds { get; set; } = 30;
+}
