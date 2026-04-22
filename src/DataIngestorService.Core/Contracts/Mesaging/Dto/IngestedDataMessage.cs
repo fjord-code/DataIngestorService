3d4d6d7a21964aa9c577e07e612e4aa3d73e73ec
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2026-04-23 00:17:49 +0300

    Added infrastructure services contracts.

---
+++ src/DataIngestorService.Core/Contracts/Mesaging/Dto/IngestedDataMessage.cs
@@ -0,0 +1,12 @@
+﻿using DataIngestorService.Core.Contracts.WeakApp.Dto;
+using System.Text.Json;
+
+namespace DataIngestorService.Core.Contracts.Mesaging.Dto;
+
+public record IngestedDataMessage
+{
+    public Guid EventId { get; init; } = Guid.NewGuid();
+    public string Type { get; init; } = null!;
+    public string Name { get; init; } = null!;
+    public JsonElement Payload { get; init; } = new JsonElement();
+};
---
+++ src/DataIngestorService.Core/Contracts/WeakApp/IWeakAppClient.cs
@@ -0,0 +1,8 @@
+﻿using DataIngestorService.Core.Contracts.WeakApp.Dto;
+
+namespace DataIngestorService.Core.Contracts.WeakApp;
+
+public interface IWeakAppClient
+{
+    Task<List<WeakAppReading>> FetchReadingsAsync(CancellationToken cancellationToken = default);
+}
