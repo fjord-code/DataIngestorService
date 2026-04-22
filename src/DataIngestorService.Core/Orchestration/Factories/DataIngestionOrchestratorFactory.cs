1fcba1eb6415eb41820db6059932a7619ccfd8e1
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2026-04-23 00:18:18 +0300

    Added the DataIngestionOrchestrator service.

---
+++ src/DataIngestorService.Core/Orchestration/Factories/DataIngestionOrchestratorFactory.cs
@@ -0,0 +1,15 @@
+﻿using DataIngestorService.Core.Base;
+using DataIngestorService.Core.Orchestration.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataIngestorService.Core.Orchestration.Factories;
+
+public interface IDataIngestionOrchestratorFactory : IAbstractFactory<IDataIngestionOrchestrator>
+{ }
+
+public sealed class DataIngestionOrchestratorFactory : AbstractFactory<IDataIngestionOrchestrator>, IDataIngestionOrchestratorFactory
+{
+    public DataIngestionOrchestratorFactory(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
+    {
+    }
+}
