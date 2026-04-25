1fac985a6f760e0cfd6558feed047233d286bfe6
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2026-04-25 13:22:37 +0300

    Fixed the health/ready endpoint.

--- src/DataIngestorService.Host/Program.cs
+++ src/DataIngestorService.Host/Program.cs
@@ -1,3 +1,5 @@
+using DataIngestorService.Core.Contracts.Options;
+using DataIngestorService.Core.Extensions.Contracts.Options;
 using DataIngestorService.Host.Workers;
 using DataIngestorService.Infrastructure;
 using HealthChecks.UI.Client;
@@ -5,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
 using OpenTelemetry.Metrics;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +17,19 @@ builder.Services.AddHostedService<DataIngestorWorker>();
 builder.Services.AddWeakAppClient(builder.Configuration);
 builder.Services.AddWolverineMessaging(builder.Host, builder.Configuration);
 
+var rabbitMqOptions = builder.Configuration
+    .GetSection(RabbitMQOptions.SectionName)
+    .Get<RabbitMQOptions>()!;
+
 builder.Services.AddHealthChecks()
     .AddCheck("live", () => Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckResult.Healthy())
-    .AddRabbitMQ();
+    .AddRabbitMQ(
+        async _ => await new ConnectionFactory
+        {
+            Uri = new Uri(rabbitMqOptions.GetConnectionString())
+        }.CreateConnectionAsync(),
+        name: "rabbitmq",
+        tags: ["ready"]);
 
 builder.Services.AddOpenTelemetry()
     .WithMetrics(m => m.AddPrometheusExporter());
@@ -30,6 +43,7 @@ var app = builder.Build();
 app.MapGet("/health/live", () => Results.Ok());
 app.MapHealthChecks("/health/ready", new HealthCheckOptions
 {
+    Predicate = check => check.Tags.Contains("ready"),
     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
 });
 
